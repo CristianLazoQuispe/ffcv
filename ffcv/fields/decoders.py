0e4b32a71575dcfe4a992a7b3d61e635482e4fc5
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-06-16 12:18:59 +0100

    Update decoders.py
    Add SimpleGrayscaleImageDecoder

--- ffcv/fields/decoders.py
+++ ffcv/fields/decoders.py
@@ -1,7 +1,7 @@
 from .basics import FloatDecoder, IntDecoder
 from .ndarray import NDArrayDecoder
-from .rgb_image import RandomResizedCropRGBImageDecoder, CenterCropRGBImageDecoder, SimpleRGBImageDecoder
+from .rgb_image import RandomResizedCropRGBImageDecoder, CenterCropRGBImageDecoder, SimpleRGBImageDecoder, SimpleGrayscaleImageDecoder
 from .bytes import BytesDecoder
 
-__all__ = ['FloatDecoder', 'IntDecoder', 'NDArrayDecoder', 'RandomResizedCropRGBImageDecoder', 
+__all__ = ['FloatDecoder', 'IntDecoder', 'NDArrayDecoder', 'RandomResizedCropRGBImageDecoder', 'SimpleGrayscaleImageDecoder',
            'CenterCropRGBImageDecoder', 'SimpleRGBImageDecoder', 'BytesDecoder']
